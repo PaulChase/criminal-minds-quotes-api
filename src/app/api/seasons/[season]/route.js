611474e8db9140a9d0d99a669e2e29b3161c42af
Paul Chase <ajonyechase@gmail.com>
2024-02-14 18:56:16 +0100

    Refactor GET method to include episode information in quotes

--- src/app/api/seasons/[season]/route.js
+++ src/app/api/seasons/[season]/route.js
@@ -2,5 +2,22 @@ import { quotes } from "@/app/data";
 
 export async function GET(request, { params }) {
 	const season = params.season;
-	return Response.json({ quotes: quotes[season] });
+
+	const allSeasonQuotes = getAllQuotesWithEpisode(quotes[season]);
+
+	return Response.json({ quotes: allSeasonQuotes });
+}
+
+function getAllQuotesWithEpisode(season) {
+	const allQuotes = [];
+	for (const episode in season) {
+		if (season.hasOwnProperty(episode)) {
+			const quotesInEpisode = season[episode];
+			quotesInEpisode.forEach((quote) => {
+				quote.episode = episode;
+				allQuotes.push(quote);
+			});
+		}
+	}
+	return allQuotes;
 }
